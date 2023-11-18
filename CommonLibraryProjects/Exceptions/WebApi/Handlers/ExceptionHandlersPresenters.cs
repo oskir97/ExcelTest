bfa02dafacc59070ad2a97a44bee347c3d4eab90
Oscar Martinez <oscarmarmar97@gmail.com>
2023-11-18 11:17:10 +0100

    Eliminación comentarios innecesarios

--- CommonLibraryProjects/Exceptions/WebApi/Handlers/ExceptionHandlersPresenters.cs
+++ CommonLibraryProjects/Exceptions/WebApi/Handlers/ExceptionHandlersPresenters.cs
@@ -4,9 +4,6 @@ namespace CommonLibraryProjects.Exceptions.WebApi.Handlers
 {
     public static class ExceptionHandlersPresenters
     {
-        //
-        // Resumen:
-        //     Gets executing assembly.
         public static Assembly Assembly => System.Reflection.Assembly.GetExecutingAssembly();
     }
 }
